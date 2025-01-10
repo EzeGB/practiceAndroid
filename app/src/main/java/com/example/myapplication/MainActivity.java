0d1a3876d2f3e7a046373c51e6262eea84f3d978
Zeke <ezequielgerstel777@gmail.com>
2025-01-10 12:41:37 -0400

    third commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -26,9 +26,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void disable (View v){
+        findViewById(R.id.button21).setEnabled(false);
+        ((Button)findViewById(R.id.button21)).setText("Disabled");
+
+//        View newView = findViewById(R.id.button21);
+//        newView.setEnabled(false);
+//        Button button = (Button) newView;
+//        ((Button) newView).setText("New Disabled");
+
+//        v.setEnabled(false);
+//        Button myButt = (Button) v;
+//        myButt.setText("Salamanca");
+//        Log.d("success", "disabled butttonn");
+
+    }
+    public void turnDisabled (View v){
         v.setEnabled(false);
-        Button myButt = (Button) v;
-        myButt.setText("Salamanca");
-        Log.d("success", "disabled butttonn");
+    }
+    public void enableDisabler(View v){
+        findViewById(R.id.button21).setEnabled(true);
+    }
+    public void change16 (View v){
+        ((Button)findViewById(R.id.button16)).setText("not16");
+        ((Button)findViewById(R.id.button17)).setText("yes17");
+    }
+    public void change17 (View v){
+        ((Button)findViewById(R.id.button16)).setText("yes16");
+        ((Button)findViewById(R.id.button17)).setText("not17");
+    }
+
+    public void disMany (View v){
+        findViewById(R.id.button19).setEnabled(false);
+        ((Button)findViewById(R.id.button19)).setText("Disabled");
+        findViewById(R.id.button20).setEnabled(false);
+        ((Button)findViewById(R.id.button20)).setText("Disabled");
+        findViewById(R.id.button25).setEnabled(false);
+        ((Button)findViewById(R.id.button25)).setText("Disabled");
+        findViewById(R.id.button26).setEnabled(false);
+        ((Button)findViewById(R.id.button26)).setText("Disabled");
     }
 }
